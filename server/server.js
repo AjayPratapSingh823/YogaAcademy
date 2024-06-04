e2ab1b4a6ad840ad6e825c479e5a706e408b0b6d
AjayPratapSingh823 <singhajaysingh545@gmail.com>
2024-06-04 13:55:33 +0530

    Server created

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const app=express();
+require('dotenv').config();
+const PORT=process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on ${PORT}`)
+})
