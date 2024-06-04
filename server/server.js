9c03fdcbf191b3538f93f5d589cafc7820b00e79
AjayPratapSingh823 <singhajaysingh545@gmail.com>
2024-06-04 13:58:10 +0530

    import mongoose

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app=express();
 require('dotenv').config();
 const PORT=process.env.PORT
+const mongoose=require('mongoose');
 
 app.listen(PORT,()=>{
     console.log(`Server is listening on ${PORT}`)
