a8147ca313d637eaf9e0c6aa8c1881a74988a3e5
Vatsal Saxena <vatsalsaxena22@gmail.com>
2024-06-08 16:00:57 +0530

    Added Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,80 +1,94 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
-import Header from './components/Header';
-import Home from './components/Home';
-import IAmNewHere from './components/IAmNewHere';
-import ContactUs from './components/ContactUs';
-import TwoZeroTwoFour from './components/TwoZeroTwoFour';
-import Blogs from './components/Blogs';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import IAmNewHere from "./components/IAmNewHere";
+import ContactUs from "./components/ContactUs";
+import TwoZeroTwoFour from "./components/TwoZeroTwoFour";
+import Blogs from "./components/Blogs";
 // Offering Classes
-import KidsClass from './components/KidsClass';
+import KidsClass from "./components/KidsClass";
 import GroupClass from "./components/GroupClass";
-import PrivateClass from './components/PrivateClass';
-import YogaWorkshops from './components/YogaWorkshops';
+import PrivateClass from "./components/PrivateClass";
+import YogaWorkshops from "./components/YogaWorkshops";
 // User
-import UserCart from "./user-components/UserCart"
-import UserOptCourses from "./user-components/UserOptCourses"
-import UserPayment from "./user-components/UserPayment"
-import UserProfile from "./user-components/UserProfile"
+import UserCart from "./user-components/UserCart";
+import UserOptCourses from "./user-components/UserOptCourses";
+import UserPayment from "./user-components/UserPayment";
+import UserProfile from "./user-components/UserProfile";
 // Admin
-import AddingClasses from "./admin-components/AddingClasses"
-import FAQs from "./admin-components/FAQs"
-import Queries from "./admin-components/Queries"
-import TrainerInfo from "./admin-components/TrainerInfo"
-import UserInfo from "./admin-components/UserInfo"
+import AddingClasses from "./admin-components/AddingClasses";
+import FAQs from "./admin-components/FAQs";
+import Queries from "./admin-components/Queries";
+import TrainerInfo from "./admin-components/TrainerInfo";
+import UserInfo from "./admin-components/UserInfo";
 // Trainer
-import TrainerBlogs from "./trainer components/TrainerBlogs"
-import TrainerProfile from "./trainer components/TrainerProfile"
+import TrainerBlogs from "./trainer components/TrainerBlogs";
+import TrainerProfile from "./trainer components/TrainerProfile";
 // Auth
-const UserLogin=React.lazy(()=>import('./auth-components/UserLogin'))
-import UserSignup from './auth-components/UserSignup';
-import ResetPassword from './auth-components/ResetPassword';
-import ForgotPassword from './auth-components/ForgotPassword';
-import UserDashboard from './user-components/UserDashboard';
+const UserLogin = React.lazy(() => import("./auth-components/UserLogin"));
+import UserSignup from "./auth-components/UserSignup";
+import ResetPassword from "./auth-components/ResetPassword";
+import ForgotPassword from "./auth-components/ForgotPassword";
+import UserDashboard from "./user-components/UserDashboard";
+import Footer from "./components/Footer";
+import OurMission from "./components/OurMission";
+import YogaDemoVideos from "./components/YogaDemoVideos";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/i-am-new-here" element={<IAmNewHere />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/two-zero-two-four" element={<TwoZeroTwoFour />} />
-        <Route path="/blogs" element={<Blogs />} />
-        {/* Offering Classes */}
-        <Route path="/kids-className" element={<KidsClass />} />
-        <Route path="/group-className" element={<GroupClass />} />
-        <Route path="/private-className" element={<PrivateClass />} />
-        <Route path="/yoga-workshops" element={<YogaWorkshops />} />
-        {/* User */}
-        <Route path='/user-dashboard' element={<UserDashboard />} />
-        <Route path='/user-cart' element={<UserCart />} />
-        <Route path='/user-opt-courses' element={<UserOptCourses />} />
-        <Route path='/user-payment' element={<UserPayment />} />
-        <Route path='/user-profile' element={<UserProfile />} />
-        {/* Admin */}
-        <Route path='/adding-classes' element={<AddingClasses />} />
-        <Route path='/faqs' element={<FAQs />} />
-        <Route path='/queries' element={<Queries />} />
-        <Route path='/trainer-info' element={<TrainerInfo />} />
-        <Route path='/user-info' element={<UserInfo />} />
-        {/* Trainer */}
-        <Route path='/trainer-blogs' element={<TrainerBlogs />} />
-        <Route path='/trainer-profile' element={<TrainerProfile />} />
-        {/* Auth */}
-        <Route path='/user-login' element={<Suspense fallback={<div>Loading...</div>}>
-      <UserLogin />
-</Suspense>} />
-        <Route path='/user-signup' element={<UserSignup />} />
-        <Route path='/forget-password' element={<ForgotPassword />} />
-        <Route path='/reset-password/:token' element={<ResetPassword />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/i-am-new-here" element={<IAmNewHere />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/two-zero-two-four" element={<TwoZeroTwoFour />} />
+          <Route path="/blogs" element={<Blogs />} />
+          {/* Offering Classes */}
+          <Route path="/kids-classes" element={<KidsClass />} />
+          <Route path="/group-classes" element={<GroupClass />} />
+          <Route path="/private-classes" element={<PrivateClass />} />
+          <Route path="/yoga-workshops" element={<YogaWorkshops />} />
+          {/* User */}
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/user-cart" element={<UserCart />} />
+          <Route path="/user-opt-courses" element={<UserOptCourses />} />
+          <Route path="/user-payment" element={<UserPayment />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+          {/* Admin */}
+          <Route path="/adding-classes" element={<AddingClasses />} />
+          <Route path="/faqs" element={<FAQs />} />
+          <Route path="/queries" element={<Queries />} />
+          <Route path="/trainer-info" element={<TrainerInfo />} />
+          <Route path="/user-info" element={<UserInfo />} />
+          {/* Trainer */}
+          <Route path="/trainer-blogs" element={<TrainerBlogs />} />
+          <Route path="/trainer-profile" element={<TrainerProfile />} />
+          {/* Auth */}
+          <Route
+            path="/user-login"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <UserLogin />
+              </Suspense>
+            }
+          />
+          <Route path="/user-signup" element={<UserSignup />} />
+          <Route path="/forget-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          {/* Footer */}
+          <Route path="/our-mission" element={<OurMission />} />
+          <Route path="/yoga-demo-videos" element={<YogaDemoVideos />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
