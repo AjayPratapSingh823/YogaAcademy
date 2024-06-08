3b6f79ce9586a541bccbd7e3cf6eef20f2ad74e2
AjayPratapSingh823 <singhajaysingh545@gmail.com>
2024-06-08 11:16:41 +0530

    user profile

--- client/src/user-components/UserProfile.jsx
+++ client/src/user-components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import css from "../css/user-profile.module.css";
 
 const UserProfile = () => {
@@ -7,15 +7,15 @@ const UserProfile = () => {
       <h2 className="text-success p-2">User Profile</h2>
       <div className="m-2 border rounded">
         <h4
-          onClick={() => setIsUploadClicked(true)}
+
           className="m-2 btn-outline-success btn"
         >
           Update Profile Pic
         </h4>
-        <div class="container p-4">
-          <div class="input-group mb-3">
-            <input type="file" class="form-control" id="inputGroupFile02" />
-            <label class="input-group-text" for="inputGroupFile02">
+        <div className="container p-4">
+          <div className="input-group mb-3">
+            <input type="file"  id="inputGroupFile02" />
+            <label className="input-group-text">
               Upload
             </label>
           </div>
@@ -26,30 +26,32 @@ const UserProfile = () => {
         <button className="btn btn-dark text-right m-2">
           <i className="fa-solid fa-pencil"></i>
         </button>
-        <div class="container p-4">
-          <div class="row">
-            <div class="col">First Name :</div>
-            <div class="col">Column</div>
+        <div className="container p-4">
+          <div className="row">
+            <div className="col">First Name :</div>
+            <div className="col">Column</div>
           </div>
-          <div class="row">
-            <div class="col">Last Name :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Last Name :</div>
+            <div className="col">Column</div>
           </div>
-          <div class="row">
-            <div class="col">Phone Number :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Phone Number :</div>
+            <div className="col">Column</div>
           </div>
-          <div class="row">
-            <div class="col">Email :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Email :</div>
+            <div className="col">Column</div>
           </div>
-          <div class="row">
-            <div class="col">Address :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Address :</div>
+            <input type="text" className="col" placeholder="Address"></input>
+            <button className="col">Save</button>
+            <button className="col">Update</button>
           </div>
-          <div class="row">
-            <div class="col">Gender :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Gender :</div>
+            <div className="col">Column</div>
           </div>
         </div>
       </div>
@@ -58,14 +60,14 @@ const UserProfile = () => {
         <button className="btn btn-dark text-right m-2">
           <i className="fa-solid fa-pencil"></i>
         </button>
-        <div class="container p-4">
-          <div class="row">
-            <div class="col">Weight :</div>
-            <div class="col">Column</div>
+        <div className="container p-4">
+          <div className="row">
+            <div className="col">Weight :</div>
+            <div className="col">Column</div>
           </div>
-          <div class="row">
-            <div class="col">Health Condition :</div>
-            <div class="col">Column</div>
+          <div className="row">
+            <div className="col">Health Condition :</div>
+            <div className="col">Column</div>
           </div>
         </div>
       </div>
