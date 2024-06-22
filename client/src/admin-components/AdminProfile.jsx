eb02ebde8b363c6d2497937546d40e0d91ba4f18
Vatsal Saxena <vatsalsaxena22@gmail.com>
2024-06-22 15:07:39 +0530

    Added img uploader

--- client/src/admin-components/AdminProfile.jsx
+++ client/src/admin-components/AdminProfile.jsx
@@ -1,77 +1,73 @@
-import React from 'react'
+import React from "react";
 import css from "../css/admin.module.css";
 
 const AdminProfile = () => {
   return (
     <div>
       <div className={css["cover"]}>
-      <div className={css["profile"]}>
+        <div className={css["profile"]}>
           <img src="" alt="" />
         </div>
       </div>
-      <h2 className='p-2'>Name <span class="badge text-bg-warning">Admin</span></h2>
-      <div className='m-2 p-2 border rounded'>
+      <h2 className="p-2">
+        Name <span class="badge text-bg-warning">Admin</span>
+      </h2>
+      <div className="m-2 p-2 border rounded">
         <h4 className="btn-success btn">Change Profile Picture</h4>
-      <div className="input-group">
-            <input type="file" className="form-control" id="inputGroupFile02" />
-            <label className="input-group-text">
-              Upload
-            </label>
-          </div>
+        <div className="input-group">
+          <input type="file" className="form-control" id="inputGroupFile02" />
+          <label className="input-group-text">Upload</label>
+        </div>
       </div>
       <div className={css["admin-details"]}>
-      <div className="m-2 border rounded">
-        <h4 className="m-2 btn-success btn">Basic Info</h4>
-        <div className="container p-4">
-          <div className="row">
-            <div className="col">Full Name:</div>
-            <div className="col m-1">Column</div>
-          </div>
-          <div className="row">
-            <div className="col">Phone Number:</div>
-            <div className="col m-1">Column</div>
-          </div>
-          <div className="row">
-            <div className="col">Email:</div>
-            <div className="col m-1">Column</div>
-          </div>
-          <div className="row">
-            <div className="col">Address:</div>
-            <div className="col">
-              <input
-                type="text"
-                className="rounded border p-1"
-                placeholder="Address"
-              ></input>
-              <button className="btn btn-dark m-1" title="Save">
-                Save
-              </button>
-              <button className="btn btn-success m-1">
-                Update
-              </button>
+        <div className="m-2 border rounded">
+          <h4 className="m-2 btn-success btn">Basic Info</h4>
+          <div className="container p-4">
+            <div className="row">
+              <div className="col">Full Name:</div>
+              <div className="col m-1">Column</div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col">Gender:</div>
-            <div className="col">
-              <input
-                type="text"
-                className="rounded border p-1"
-                placeholder="Gender"
-              ></input>
-              <button className="btn btn-dark m-1" title="Save">
-                Save
-              </button>
-              <button className="btn btn-success m-1">
-                Update
-              </button>
+            <div className="row">
+              <div className="col">Phone Number:</div>
+              <div className="col m-1">Column</div>
+            </div>
+            <div className="row">
+              <div className="col">Email:</div>
+              <div className="col m-1">Column</div>
+            </div>
+            <div className="row">
+              <div className="col">Address:</div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="rounded border p-1"
+                  placeholder="Address"
+                ></input>
+                <button className="btn btn-dark m-1" title="Save">
+                  Save
+                </button>
+                <button className="btn btn-success m-1">Update</button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">Gender:</div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="rounded border p-1"
+                  placeholder="Gender"
+                ></input>
+                <button className="btn btn-dark m-1" title="Save">
+                  Save
+                </button>
+                <button className="btn btn-success m-1">Update</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminProfile
+export default AdminProfile;
--- client/src/blog/Blog.jsx
+++ client/src/blog/Blog.jsx
@@ -1,149 +1,155 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import '../App.css'; // Ensure custom CSS is imported
-import axios from 'axios'
+import React, { useState, useRef, useEffect } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "../App.css"; // Ensure custom CSS is imported
+import axios from "axios";
 
 const Blog = () => {
-  const [editorContent, setEditorContent] = useState('');
+  const [editorContent, setEditorContent] = useState("");
   const quillRef = useRef(null);
 
   useEffect(() => {
     const quill = quillRef.current.getEditor();
     const handleClick = (event) => {
       const img = event.target;
-      if (img.tagName === 'IMG') {
-        img.setAttribute('data-resizable', 'true');
+      if (img.tagName === "IMG") {
+        img.setAttribute("data-resizable", "true");
       }
     };
 
     const handleMouseDown = (event) => {
       const img = event.target;
-      if (img.getAttribute('data-resizable')) {
-        img.classList.add('resizing');
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp);
+      if (img.getAttribute("data-resizable")) {
+        img.classList.add("resizing");
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
       }
     };
 
     const handleMouseMove = (event) => {
-      const img = document.querySelector('.resizing');
+      const img = document.querySelector(".resizing");
       if (img) {
-        img.style.width = event.pageX - img.getBoundingClientRect().left + 'px';
-        img.style.height = 'auto';
+        img.style.width = event.pageX - img.getBoundingClientRect().left + "px";
+        img.style.height = "auto";
       }
     };
 
     const handleMouseUp = () => {
-      const img = document.querySelector('.resizing');
+      const img = document.querySelector(".resizing");
       if (img) {
-        img.classList.remove('resizing');
-        document.removeEventListener('mousemove', handleMouseMove);
-        document.removeEventListener('mouseup', handleMouseUp);
+        img.classList.remove("resizing");
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
       }
     };
 
-    quill.root.addEventListener('click', handleClick);
-    quill.root.addEventListener('mousedown', handleMouseDown);
+    quill.root.addEventListener("click", handleClick);
+    quill.root.addEventListener("mousedown", handleMouseDown);
 
     return () => {
-      quill.root.removeEventListener('click', handleClick);
-      quill.root.removeEventListener('mousedown', handleMouseDown);
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
+      quill.root.removeEventListener("click", handleClick);
+      quill.root.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
     };
   }, []);
 
-
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const title=e.target.title.value;
-    const desc=e.target.desc.value;
-    const date=e.target.date.value;
-    const content=editorContent;
+    const title = e.target.title.value;
+    const desc = e.target.desc.value;
+    const date = e.target.date.value;
+    const content = editorContent;
     const formData = new FormData();
-    formData.append('title',title);
-    formData.append('desc',desc);
-    formData.append('data',date);
-    formData.append('content',content);
+    formData.append("title", title);
+    formData.append("desc", desc);
+    formData.append("data", date);
+    formData.append("content", content);
 
-
-    const parser= new DOMParser();
-    const doc= parser.parseFromString(content,'text/html');
-    const images=doc.querySelectorAll('img');
-    for(const img of images){
-        const imageUrl=img.src;
-        const imageBlob=await fetch(imageUrl).then(res=>res.blob());
-        formData.append('images',imageBlob,'image.png');
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(content, "text/html");
+    const images = doc.querySelectorAll("img");
+    for (const img of images) {
+      const imageUrl = img.src;
+      const imageBlob = await fetch(imageUrl).then((res) => res.blob());
+      formData.append("images", imageBlob, "image.png");
     }
-    await axios.post('/blogs',formData,{
-        headers:{'Content-Type':'multipart/form-data'},
-    })
-  }
+    await axios.post("/blogs", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  };
 
   const modules = {
     toolbar: [
-      [{ 'header': '1'}, { 'header': '2'}, { 'font': [] }],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      ['bold', 'italic', 'underline'],
-      ['link', 'image'],
-      [{ 'align': [] }],
-      ['clean']
-    ]
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["bold", "italic", "underline"],
+      ["link", "image"],
+      [{ align: [] }],
+      ["clean"],
+    ],
   };
 
   const formats = [
-    'header', 'font',
-    'list', 'bullet',
-    'bold', 'italic', 'underline',
-    'link', 'image', 'align'
+    "header",
+    "font",
+    "list",
+    "bullet",
+    "bold",
+    "italic",
+    "underline",
+    "link",
+    "image",
+    "align",
   ];
 
   return (
     <div>
-      <form action="" onSubmit={handleSubmit} className='m-2 p-2 border rounded'>  
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="m-2 p-2 border rounded"
+      >
+        <div className="input-group">
+          <span className="input-group-text" id="basic-addon1">
+            Title
+          </span>
+          <input type="text" className="form-control" name="title" />
+        </div>
 
-<div className="input-group">
-  <span className="input-group-text" id="basic-addon1">
-    Title
-  </span>
-  <input type="text" className="form-control" name="title" />
-</div>
+        <div className="input-group">
+          <input type="file" className="form-control" id="inputGroupFile02" />
+          <label className="input-group-text">Upload</label>
+        </div>
 
-<div className="input-group">
-  <span className="input-group-text">Description</span>
-  <textarea className="form-control" name="desc"></textarea>
-</div>
+        <div className="input-group">
+          <span className="input-group-text">Description</span>
+          <textarea className="form-control" name="desc"></textarea>
+        </div>
 
-<div className="input-group">
-  <span className="input-group-text" id="basic-addon1">
-    Date
-  </span>
-  <input
-    type="date"
-    className="form-control"
-    name="date"
-  />
-</div>
+        <div className="input-group">
+          <span className="input-group-text" id="basic-addon1">
+            Date
+          </span>
+          <input type="date" className="form-control" name="date" />
+        </div>
 
+        <ReactQuill
+          ref={quillRef}
+          value={editorContent}
+          onChange={setEditorContent}
+          modules={modules}
+          formats={formats}
+          theme="snow"
+          className="quill-editor"
+        />
 
-
-      <ReactQuill 
-        ref={quillRef}
-        value={editorContent}
-        onChange={setEditorContent}
-        modules={modules}
-        formats={formats}
-        theme="snow"
-        className="quill-editor" 
-      />
-
-<div className="text-center m-2">
-<button className="btn btn-dark" type="submit">
-  Post <i className='fa-solid fa-paper-plane'></i>
-</button>
-</div>
-</form>
+        <div className="text-center m-2">
+          <button className="btn btn-dark" type="submit">
+            Post <i className="fa-solid fa-paper-plane"></i>
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
