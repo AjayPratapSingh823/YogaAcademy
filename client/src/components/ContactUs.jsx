68fa6234a55d71971b99977f7088c5f144f79d0c
AjayPratapSingh823 <singhajaysingh545@gmail.com>
2024-06-24 15:28:27 +0530

    commit

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -26,15 +26,15 @@ const ContactUs = () => {
 
   return (
     <>
-    <section class={css["contactHeader"]}>
-    <div class={css["contactOuter"]}>
+    <section className={css["contactHeader"]}>
+    <div className={css["contactOuter"]}>
       <h1>Get In Touch With Us</h1>
       <p>
         Our yoga classes are hot, warm, or non-heated and we offer a variety
         of styles and approaches to suit everyone’s unique needs.
       </p>
     </div>
-    <div class ={css["contactOuter"]}>
+    <div className ={css["contactOuter"]}>
       <form action="">
         <h2>Fill up the form below</h2>
         <input type="text" onChange={(e)=>setFullname(e.target.value)} placeholder="Name" id="uname" />
@@ -47,20 +47,20 @@ const ContactUs = () => {
   </section>
 
   {/* contact Info Section */}
-  <section class={css["contactInfo"]}>
-    <div class={css["ContactInfoLap"]}>
-      <div class={css["cInfo"]}>
-        <i class="fa-solid fa-phone"></i>
+  <section className={css["contactInfo"]}>
+    <div className={css["ContactInfoLap"]}>
+      <div className={css["cInfo"]}>
+        <i className="fa-solid fa-phone"></i>
         <h5>PHONE</h5>
         <p>480-519-6276</p>
       </div>
-      <div class={css["cInfo"]}>
-        <i class="fa-solid fa-envelope"></i>
+      <div className={css["cInfo"]}>
+        <i className="fa-solid fa-envelope"></i>
         <h5>EMAIL</h5>
         <p>info@example.com</p>
       </div>
-      <div class={css["cInfo"]}>
-        <i class="fa-solid fa-location-dot"></i>
+      <div className={css["cInfo"]}>
+        <i className="fa-solid fa-location-dot"></i>
         <h5>ADDRESS</h5>
         <p>51 211th Street, Bayside, NY</p>
       </div>
