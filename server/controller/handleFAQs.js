a0da1394327c04f4e9c51b9a7bccc7cf19b7f391
Vatsal Saxena <vatsalsaxena22@gmail.com>
2024-06-27 14:10:14 +0530

    recommit

--- server/controller/handleFAQs.js
+++ server/controller/handleFAQs.js
@@ -1,28 +1,30 @@
-const FAQsModel = require("../models/FAQs")
+const FAQsModel = require("../models/FAQs");
 
 const handleFAQs = async (req, res) => {
-    try {
-        const {ques, ans} = req.body
-
+    const { ques, ans } = req.body;
+  try {
+    if(!ques){
+        return res.status(203).send("Enter your question and then submit!")
+    }
     const quesData = new FAQsModel({
-        ques,
-        ans,
-    })
+      ques,
+      ans,
+    });
 
-    await quesData.save()
-    res.status(200).send("Your Question Saved!")
-    } catch (error) {
-        res.status(203).send(error)
-    }
-}
+    await quesData.save();
+    res.status(200).send("Your Question Saved!");
+  } catch (error) {
+    res.status(203).send(error);
+  }
+};
 
-const handleFetchFAQs = async (req, res)=>{
-    try {
-        const FAQSArr = await FAQsModel.find()
-        res.status(200).send(FAQSArr)
-    } catch (error) {
-        res.status(200).send(error)
-    }
-}
+const handleFetchFAQs = async (req, res) => {
+  try {
+    const FAQSArr = await FAQsModel.find();
+    res.status(200).send(FAQSArr);
+  } catch (error) {
+    res.status(200).send(error);
+  }
+};
 
-module.exports = {handleFAQs, handleFetchFAQs}
+module.exports = { handleFAQs, handleFetchFAQs };
